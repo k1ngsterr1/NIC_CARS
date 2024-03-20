85f22cbb724db6d45c179cf713c91cceb9801ec6
senseiGai <swerss564@gmail.com>
2024-03-20 21:14:20 +0600

    version 1.0.3

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,20 +2,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          darken: "#1A202C",
-          "red-text": "#FF4423",
-          "white-text": "",
-        },
-        indicators: {
-          "light-dark-text": "",
-          "gradient-yellow": "",
-        },
-        button: {
-          "bg-button": "",
-        },
-      },
+      colors: {},
     },
   },
   plugins: [],
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
+});
