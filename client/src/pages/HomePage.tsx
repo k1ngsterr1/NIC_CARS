0c9801f28ab0a0ad87504592cbd0af4798516c6f
senseiGai <swerss564@gmail.com>
2024-03-20 21:09:07 +0600

    version 1.0.1

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface RootState {
+  modalSlice: {
+    isShown: boolean;
+  };
+}
+
+const HomePage = () => {
+  return <div></div>;
+};
+
+export default HomePage;
