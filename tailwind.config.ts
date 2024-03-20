2f010f3d5d512760fd47a6aac9426eb9c6cb4595
senseiGai <swerss564@gmail.com>
2024-03-20 20:51:16 +0600

    version 1.0.0

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          darken: "#1A202C",
+          "red-text": "#FF4423",
+          "white-text": "",
+        },
+        indicators: {
+          "light-dark-text": "",
+          "gradient-yellow": "",
+        },
+        button: {
+          "bg-button": "",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
